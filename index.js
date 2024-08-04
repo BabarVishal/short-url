f4b31aac03ee9c23350d84134ff66a94bc8fa055
vishal <vishalbabar7351@gmail.com>
2024-08-04 15:03:33 +0530

    added ejs for clint side rendring

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
+const staticROutes = require("./routes/stasticROuter")
+
+//For Frentend...
+const path = require("path");
 
 const app = express();
 const PORT = 8001;
@@ -10,11 +14,22 @@ connectToMongoDB("mongodb://127.0.0.1:27017/my-Url").then(() =>
   console.log("Mongodb connected")
 );
 
-app.use(express.json());
+//For Frentend...
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//routes
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+//staticROutes
+app.use("/", staticROutes)
+
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
